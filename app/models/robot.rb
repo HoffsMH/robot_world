3c096d960a080ca75089501d4ebcc0549b0f50df
HoffsMH <hecker.matthew@gmail.com>
2015-08-25 21:47:41 -0600

    added test scaffolding

---
+++ app/models/robot.rb
@@ -0,0 +1,10 @@
+class Robot
+  def initialize(args)
+   @name = args[:name]
+   @city = args[:city]
+   @state = args[:state]
+   @birthday= arg[:birthday]
+   @date_hired= args[:date_hired]
+   @department = [:department]
+  end
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,4 +4,39 @@ SimpleCov.start
 require 'pry'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'fileutils'
 
+
+$LOAD_PATH.unshift(File.expand_path(File.dirname(__FILE__) + "/.."))
+
+
+def sample_robots
+[
+  {
+    name: "Matt",
+    city: "Denver",
+    state: "CO",
+    birthdate: "07-04-1987",
+    date_hired: "08-24-2015",
+    department: "software"
+  },
+
+  {
+    name: "Nick",
+    city: "Lafayette",
+    state: "LA",
+    birthdate: "23-09-1987",
+    date_hired: "08-24-2015",
+    department: "finance"
+  },
+
+  {
+    name: "Ben",
+    city: "Lafayette",
+    state: "LA",
+    birthdate: "23-09-1967",
+    date_hired: "08-24-2012",
+    department: "software"
+  }
+]
+end
